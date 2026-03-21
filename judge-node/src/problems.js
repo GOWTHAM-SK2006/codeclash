345b71857f9cc54e85e475ee1fed82dc9178045d
GOWTHAM-SK2006 <surulivelgowtham@gmail.com>
2026-03-21 15:32:38 +0530

    Revert "chore: remove all demo/sample problems from judge-node/src/problems.js (no problems anywhere)"
    This reverts commit 37edbd87be3909aece75fb0a0e49c68570e68a48.

--- judge-node/src/problems.js
+++ judge-node/src/problems.js
@@ -1,2 +1,16 @@
 // Problem storage for CodeClash Judge
-module.exports = [];
+module.exports = [
+  {
+    id: "two-sum",
+    title: "Two Sum",
+    functionName: "twoSum",
+    language: "python",
+    description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
+    constraints: "1 <= nums.length <= 10^4, -10^9 <= nums[i] <= 10^9, -10^9 <= target <= 10^9",
+    testcases: [
+      { input: "[[2,7,11,15], 9]", expected: "[0, 1]", visible: true },
+      { input: "[[3,2,4], 6]", expected: "[1, 2]", visible: true },
+      { input: "[[3,3], 6]", expected: "[0, 1]", visible: false }
+    ]
+  }
+];
